9a1505c525a66fb056e10d918e8f52652a16a5e1
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-09-07 11:16:44 -0300

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include "class.h"
 
 using namespace std;
 
 int main()
 {
-    cout<<"Hello, world!"<<endl;
+    cout << "Hello World" << endl;
+    MyClass my_class(7);
+    my_class.printAttrib();
+    int *x = my_class.getAttribAddress();
+    cout << *x << endl;
+    *x = 21; //breaks encapsulation
+    my_class.printAttrib();
 
     return 0;
 }
